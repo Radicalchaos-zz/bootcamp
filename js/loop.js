7bc776575bd3dbe08548f283fdc42c88998bac98
Radicalchaos <30750791+Radicalchaos@users.noreply.github.com>
2018-04-17 11:23:04 +0200

    Another simple tast using loops from JS - numbers console.log

---
+++ js/loop.js
@@ -0,0 +1,25 @@
+console.log("first -10 to 19")
+var counter = -10
+
+while (counter < 20) {
+  console.log(counter);
+  counter++;
+}
+console.log("second 10 and 40")
+var count =10
+
+ while (count <= 40) {
+   console.log(count);
+   count+=2
+ }
+
+ console.log("odd between 300 and 333")
+
+var counts=300
+
+ while (counts <= 333); {
+  if (counts % 2 !=== 0) {
+    console.log (counter);
+  }
+  counter+=1;
+ }
